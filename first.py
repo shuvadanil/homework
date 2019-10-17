5b481259b432d431b2486ff506d2d38458c6c4b3
shuvadanil <38698781+shuvadanil@users.noreply.github.com>
2019-10-17 10:02:42 +0300

    Shuvalov Danil AI 182
    Laba 1

--- first.py
+++ first.py
@@ -1,30 +1,56 @@
-from random import randrange
-import time
-from datetime import datetime
+x = 1
+y = 2
+z = 5
 
-#buble
-my_list = [ randrange(0, 15) for i in range(10) ]
+if z <= 1 :
+    print("Hey")
+else:
+    print("Ho")
+ #Shuvalov Danil Ai-182
 
-max_list = len( my_list )
+#Сортировка 1
+def BubbleSort(someArray):
+    for i in range(0, len(someArray) - 1):
+        swapCheck = False
+        for j in range(len(someArray) - i - 1):
+            if (someArray[j] > someArray[j + 1]):
+                someArray[j], someArray[j + 1] = someArray[j + 1], someArray[j]
+                SswapCheck = True
+    if (not swapCheck): 
+        swapCheck = False
+    return someArray
 
-i = 0
-while i < max_list:
+myArray = [234, 655, 618, 687, 524, 698, 947, 332, 890, 124, 508, 809, 325, 456, 344]
+print(BubbleSort(myArray))
 
-    j = 0
-    while j < max_list-i-1:
+#Сортировка 2
+def SelectionSort(someArray):
+    for i in range(len(someArray) - 1):
+        minElement = i
+        for j in range(i + 1, len(someArray)):
+            if (someArray[j] < someArray[minElement]):
+                minElement = j
+        if minElement != i:
+            someArray[i], someArray[minElement] = someArray[minElement], someArray[i]
+    return someArray
 
-        if my_list[ j ] > my_list[ j + 1 ]:
+anotherMyArray = [342, 231, 231, 602, 23, 312, 540, 233, 534, 472, 544, 233, 968, 423, 114]
+taskFromPractice = [21, 34, 19, 65, 35, 7, 45, 43, 76]
+print(SelectionSort(anotherMyArray))
+print(SelectionSort(taskFromPractice))
 
-            my_list[ j ], my_list[ j + 1] = my_list[ j + 1], my_list[ j ]
-        j+=1
-    i += 1
+#Сортировка 3
+def InsertionSort(someArray):
+    for i in range(1, len(someArray)):
+        checkingNumber = someArray[i]
+        j = i - 1
+        while j >= 0 and checkingNumber < someArray[j]:
+            someArray[j + 1] = someArray[j]
+            j -= 1
+        someArray[j + 1] = checkingNumber
+    return someArray
 
-print( my_list )
-
-
-
-from datetime import datetime
-start_time = datetime.now()
-# do your work here
-end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
+andAnotherAnotherMyArray = [456, 234, 123, 243, 652, 339, 323, 432, 383, 967, 123, 225, 174, 234, 411]
+anotherTaskFromPractice = [34, 21, 45, 67, 34, 78, 53, 54, 87]
+print(InsertionSort(andAnotherAnotherMyArray))
+print(InsertionSort(anotherTaskFromPractice))
